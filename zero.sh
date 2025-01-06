ee5e93831e66cff8c9b089792ac5f9f85ba40193
Andikapratama1029 <168614929+Andikapratama1029@users.noreply.github.com>
2025-01-07 05:44:49 +0700

    Update zero.sh

--- zero.sh
+++ zero.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ADMIN_WHATSAPP_NUMBER="081997715124"
+ADMIN_WHATSAPP_NUMBER="085789202053"
 
 WHATSAPP_FILE="/var/whatsapp_number.txt"
 LICENSE_FILE="/var/license.txt"
@@ -55,9 +55,9 @@ echo "╭╯━╰━┫╰━━┫┃┃╰┫╰━━╯┃"
 echo "╰━━━━┻━━━┻╯╰━┻━━━━╯"
 echo -e "\033[0m"
 echo -e "${RED}𝗢𝗪𝗡𝗘𝗥${RESET}${BLUE}𝗗𝗘𝗩𝗘𝗟𝗢𝗣𝗘𝗥${RESET}"
-echo -e "\033[32mWHATSAPP ZERODEV : 081997715124\033[0m"
-echo -e "\033[31mTELEGRAM : t.me/ZerooHosting\033[0m"
-echo -e "${RED} © ZERO-DEVELOPER"
+echo -e "\033[32mWHATSAPP DREAM STALKER : 085789202053\033[0m"
+echo -e "\033[31mTELEGRAM : t.me/DREAM STALKER\033[0m"
+echo -e "${RED} © DREAM STALKER"
 echo ""
 
 # Tambahkan bagian lain dari skrip Anda di sini jika diperlukan
